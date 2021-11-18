49c52611dbe99fa66e7ddce42cc9c719573ad08b
Deniz Setyawan <denizsetyawan95@gmail.com>
2021-11-18 09:29:37 +0700

    add product -a

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,15 +1,44 @@
+import { useState } from "react"
+import { useHistory } from "react-router-dom"
+import axios from "axios"
+
 const AddProduct = () => {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const history = useHistory();
+    
+    const saveProduct = async (e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:5000/products', {
+            title: title,
+            price: price
+        });
+        history.push('/');
+    }
+    
     return (
         <div>
-            <form>
+            <form onSubmit={ saveProduct }>
                 <div className="field">
                     <label className="label">Title</label>
-                    <input type="text" className="input" placeholder="Title" />
+                    <input 
+                    type="text" 
+                    className="input" 
+                    placeholder="Title"
+                    value={ title }
+                    onChange={ (e) => setTitle(e.target.value) }
+                    />
                 </div>
                 
                 <div className="field">
                     <label className="label">Price</label>
-                    <input type="text" className="input" placeholder="Price" />
+                    <input 
+                    type="text" 
+                    className="input" 
+                    placeholder="Price" 
+                    value={ price }
+                    onChange={ (e) => setPrice(e.target.value) }
+                    />
                 </div>
                 
                 <div className="field">
